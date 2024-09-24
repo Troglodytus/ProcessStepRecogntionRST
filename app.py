b092c9026aa96cd3677fdb1c1721272977a00e71
Matteo <langer.matteo@gmail.com>
2024-09-24 12:32:19 +0200

    mongodb implementation

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 from flask import Flask
 from flask import Response
 from flask import render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
-
+client = MongoClient("mongodb://localhost:27017/")
+db = client.test_database
 @app.route('/')
 def home():
     return "Hallo, Flask!"
+
+@app.route("/check_db")
+def check_db():
+    try:
+        # Überprüfe die Verbindung zur Datenbank
+        client.admin.command('ping')
+        return "Verbindung besteht!"
+    except Exception as e:
+        return f"Verbindung fehlgeschlagen: {e}"
 
 @app.route("/healthz")
 def healthz():
